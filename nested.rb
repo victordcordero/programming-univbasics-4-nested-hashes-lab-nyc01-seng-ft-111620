45123c6c7fbb063f8ab34ec5bdc4b4e839b4095f
Victor Cordero <victordcordero@gmail.com>
2020-11-02 16:40:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -40,7 +40,7 @@ def alan_kay_is_known_for
 
 end
 
-alan_kay_is_known_for[:programmer_hash][:alan_kay][:known_for]
+programmer_hash][:alan_kay][:known_for]
 
 def dennis_ritchies_language
 	programmer_hash =
